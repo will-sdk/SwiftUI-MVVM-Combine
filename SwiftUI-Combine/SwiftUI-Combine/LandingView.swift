be3705d84e57d8edaf4f33395038cf7eb908ab65
Willy-SDK <>
2022-11-08 18:08:02 +0000

    -Adjust Ui

--- SwiftUI-Combine/SwiftUI-Combine/LandingView.swift
+++ SwiftUI-Combine/SwiftUI-Combine/LandingView.swift
@@ -11,20 +11,37 @@ struct LandingView: View {
     var body: some View {
         GeometryReader { proxy in
             VStack {
-                Spacer().frame(height: proxy.size.height * 0.25)
+                Spacer().frame(height: proxy.size.height * 0.18)
                 Text("Tickets")
                     .font(.system(size: 64,
                                   weight: .medium))
                     .foregroundColor(.white)
                 Spacer()
+                Button(action: {}) {
+                    HStack(spacing: 15) {
+                        Spacer()
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 24, weight: .semibold))
+                            .foregroundColor(.white)
+                        Text("Search Tickets")
+                            .font(.system(size: 24, weight: .semibold))
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                }.padding(.horizontal, 15)
+                .buttonStyle(PrimaryButtonStyle())
             }.frame(
                 maxWidth: .infinity,
                 maxHeight:  .infinity
             )
             .background(Image("stadium")
                 .resizable()
-                .aspectRatio(contentMode: .fill)
-            ).edgesIgnoringSafeArea(.all)
+                .aspectRatio(
+                    contentMode: .fill
+                ).overlay(Color.black.opacity(0.4))
+                .frame(width: proxy.size.width)
+                .edgesIgnoringSafeArea(.all)
+            )
         }
     }
 }
