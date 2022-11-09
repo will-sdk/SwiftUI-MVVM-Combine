2cbb3f5216d721b236a7a7660e14fd7877073ad7
Willy-SDK <>
2022-11-09 17:56:30 +0000

    -Implement navigation

--- SwiftUI-Combine/SwiftUI-Combine/LandingView.swift
+++ SwiftUI-Combine/SwiftUI-Combine/LandingView.swift
@@ -8,41 +8,48 @@
 import SwiftUI
 
 struct LandingView: View {
+    @State private var isActive = false
     var body: some View {
-        GeometryReader { proxy in
-            VStack {
-                Spacer().frame(height: proxy.size.height * 0.18)
-                Text("Tickets")
-                    .font(.system(size: 64,
-                                  weight: .medium))
-                    .foregroundColor(.white)
-                Spacer()
-                Button(action: {}) {
-                    HStack(spacing: 15) {
-                        Spacer()
-                        Image(systemName: "plus.circle")
-                            .font(.system(size: 24, weight: .semibold))
-                            .foregroundColor(.white)
-                        Text("Search Tickets")
-                            .font(.system(size: 24, weight: .semibold))
-                            .foregroundColor(.white)
-                        Spacer()
+        NavigationView {
+            GeometryReader { proxy in
+                VStack {
+                    Spacer().frame(height: proxy.size.height * 0.18)
+                    Text("Tickets")
+                        .font(.system(size: 64,
+                                      weight: .medium))
+                        .foregroundColor(.white)
+                    Spacer()
+                    NavigationLink(destination: SelectTeam(), isActive: $isActive) {
+                        Button(action: {
+                            isActive = true
+                        }) {
+                            HStack(spacing: 15) {
+                                Spacer()
+                                Image(systemName: "plus.circle")
+                                    .font(.system(size: 24, weight: .semibold))
+                                    .foregroundColor(.white)
+                                Text("Search Tickets")
+                                    .font(.system(size: 24, weight: .semibold))
+                                    .foregroundColor(.white)
+                                Spacer()
+                            }
+                        }.padding(.horizontal, 15)
+                            .buttonStyle(PrimaryButtonStyle())
                     }
-                }.padding(.horizontal, 15)
-                .buttonStyle(PrimaryButtonStyle())
-            }.frame(
-                maxWidth: .infinity,
-                maxHeight:  .infinity
-            )
-            .background(Image("stadium")
-                .resizable()
-                .aspectRatio(
-                    contentMode: .fill
-                ).overlay(Color.black.opacity(0.4))
-                .frame(width: proxy.size.width)
-                .edgesIgnoringSafeArea(.all)
-            )
-        }
+                }.frame(
+                    maxWidth: .infinity,
+                    maxHeight:  .infinity
+                )
+                .background(Image("stadium")
+                    .resizable()
+                    .aspectRatio(
+                        contentMode: .fill
+                    ).overlay(Color.black.opacity(0.4))
+                    .frame(width: proxy.size.width)
+                    .edgesIgnoringSafeArea(.all)
+                )
+            }
+        }.accentColor(.primary)
     }
 }
 
---
+++ SwiftUI-Combine/SwiftUI-Combine/SelectTeam.swift
@@ -0,0 +1,16 @@
+//
+//  SelectTeam.swift
+//  SwiftUI-Combine
+//
+//  Created by Willy on 09/11/2022.
+//
+
+import SwiftUI
+
+struct SelectTeam: View {
+    var body: some View {
+        VStack {
+            
+        }.navigationTitle("Select Team")
+    }
+}
