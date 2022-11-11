97e2f97910501cd15e65f49a57e6149e2b780907
Willy-SDK <>
2022-11-11 18:07:23 +0000

    -fix ui landing page

--- SwiftUI-Combine/SwiftUI-Combine/LandingView.swift
+++ SwiftUI-Combine/SwiftUI-Combine/LandingView.swift
@@ -13,7 +13,7 @@ struct LandingView: View {
         NavigationView {
             GeometryReader { proxy in
                 VStack {
-                    Spacer().frame(height: proxy.size.height * 0.18)
+                    Spacer().frame(height: proxy.size.height * 0.11)
                     Text("Tickets")
                         .font(.system(size: 64,
                                       weight: .medium))
