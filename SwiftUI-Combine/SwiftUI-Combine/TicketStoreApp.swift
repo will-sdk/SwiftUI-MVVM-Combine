ee06c07a5f04d1d8288d6b3a6e1e9974e9bd565a
Willy-SDK <>
2022-11-14 18:33:07 +0000

    -init firebase

--- SwiftUI-Combine/SwiftUI-Combine/TicketStoreApp.swift
+++ SwiftUI-Combine/SwiftUI-Combine/TicketStoreApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct TicketStoreApp: App {
+    
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             LandingView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
