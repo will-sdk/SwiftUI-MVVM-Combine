45f20af2f8b9dd4e26cc204096ba6e4249a3a9fd
Willy-SDK <>
2022-11-17 17:45:44 +0000

    -Add ticket service

---
+++ SwiftUI-Combine/SwiftUI-Combine/Services/TicketService.swift
@@ -0,0 +1,29 @@
+//
+//  TicketService.swift
+//  SwiftUI-Combine
+//
+//  Created by Willy on 17/11/2022.
+//
+
+import Foundation
+import Combine
+import FirebaseFirestoreSwift
+import FirebaseFirestore
+
+protocol TicketServiceProtocol {
+    func create(_ ticket: Ticket) -> AnyPublisher<Void, Error>
+}
+
+final class TicketService: TicketServiceProtocol {
+    private let db = Firestore.firestore()
+    func create(_ ticket: Ticket) -> AnyPublisher<Void, Error> {
+        return Future<Void, Error> { promise in
+            do {
+                _ = try self.db.collection("tickets").addDocument(from: ticket)
+                promise(.success(()))
+            } catch {
+                promise(.failure(error))
+            }
+        }.eraseToAnyPublisher()
+    }
+}
