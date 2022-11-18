5f7036f9e84eb3977b71d2f8ad5fd8344b11cc8e
Willy-SDK <>
2022-11-18 20:44:34 +0000

    -refactor TicketService error

--- SwiftUI-Combine/SwiftUI-Combine/Services/TicketService.swift
+++ SwiftUI-Combine/SwiftUI-Combine/Services/TicketService.swift
@@ -11,24 +11,24 @@ import FirebaseFirestoreSwift
 import FirebaseFirestore
 
 protocol TicketServiceProtocol {
-    func create(_ ticket: Ticket) -> AnyPublisher<Void, Error>
+    func create(_ ticket: Ticket) -> AnyPublisher<Void, TicketError>
 }
 
 final class TicketService: TicketServiceProtocol {
     private let db = Firestore.firestore()
-    func create(_ ticket: Ticket) -> AnyPublisher<Void, Error> {
-        return Future<Void, Error> { promise in
+    func create(_ ticket: Ticket) -> AnyPublisher<Void, TicketError> {
+        return Future<Void, TicketError> { promise in
             do {
                 _ = try self.db.collection("tickets").addDocument(from: ticket) { error in
                     if let error = error {
-                        promise(.failure(error))
+                        promise(.failure(.default(description: error.localizedDescription)))
                     } else {
                         promise(.success(()))
                     }
                 }
                 promise(.success(()))
             } catch {
-                promise(.failure(error))
+                promise(.failure(.default()))
             }
         }.eraseToAnyPublisher()
     }
